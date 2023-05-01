852550ba22b81bada8e758c62fe6ba82a5511090
emhaki <emhaki@emhakiui-MacBookAir.local>
2023-05-01 15:58:12 +0900

    주석

---
+++ Test/src/chap_01/_02_Comment.java
@@ -0,0 +1,13 @@
+package chap_01;
+
+public class _02_Comment {
+    public static void main(String[] args) {
+        System.out.println("(10분 전) 잠시 후 결혼식 시작 예정입니다. 자리에 앉아주세요.");
+        System.out.println("(5분 전) 잠시 후 결혼식 시작 예정입니다. 자리에 앉아주세요.");
+        System.out.println("잠시 후 결혼식 시작 예정입니다. 자리에 앉아주세요.");
+
+        int size = 120;
+        size += 10; // 어린이 신발 사이즈 증가
+        System.out.println("신발 사이즈 " + size + " 보여주세요");
+    }
+}
