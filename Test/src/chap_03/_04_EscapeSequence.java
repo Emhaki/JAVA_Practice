5c58ea1b41a6d4eb8486d4008d3ca4cf72eea017
emhaki <mhmh779@naver.com>
2023-05-02 21:34:54 +0900

    EscapeSequence

---
+++ Test/src/chap_03/_04_EscapeSequence.java
@@ -0,0 +1,12 @@
+package chap_03;
+
+public class _04_EscapeSequence {
+    public static void main(String[] args) {
+        System.out.println("자바\n공부중");
+        // 자바
+        // 공부중
+
+        // \t : 탭
+        System.out.println("해물파전\t9000원");
+    }
+}
