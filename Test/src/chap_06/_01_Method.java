e5eccf4f4ecedafe6fbb4ac073cac87f5488ae0b
emhaki <mhmh779@naver.com>
2023-05-05 16:12:56 +0900

    Method

---
+++ Test/src/chap_06/_01_Method.java
@@ -0,0 +1,14 @@
+package chap_06;
+
+public class _01_Method {
+    // 메소드 정의
+    public static void sayHello() {
+        System.out.println("Hello I'm Method!");
+    };
+
+    public static void main(String[] args) {
+        // 메소드 호출 
+        sayHello();
+        
+    }
+}
