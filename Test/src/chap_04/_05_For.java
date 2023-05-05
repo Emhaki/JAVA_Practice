2c0917a70b28d04dccf47ef3b63b58b449f8b8cc
emhaki <mhmh779@naver.com>
2023-05-05 12:51:53 +0900

    For

---
+++ Test/src/chap_04/_05_For.java
@@ -0,0 +1,15 @@
+package chap_04;
+
+public class _05_For {
+    public static void main(String[] args)
+    {
+        // 반복문
+        System.out.println("어서오세요, 이명학입니다.");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("안녕하세요 ㅎㅎ" + i);
+        }
+        for (int i = 5; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
