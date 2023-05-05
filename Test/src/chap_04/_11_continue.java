8ffd1907496b7abcca143f67eb23500ec3a2cd9a
emhaki <mhmh779@naver.com>
2023-05-05 13:41:28 +0900

    Continue

---
+++ Test/src/chap_04/_11_continue.java
@@ -0,0 +1,24 @@
+package chap_04;
+
+public class _11_continue {
+    public static void main(String[] args) {
+        // Continue
+
+        int max = 20;
+        int sold = 0;
+        int noShow = 17;
+        for (int i = 1; i <= 50; i++) {
+            System.out.println(i + "번 손님, 치킨 나왔습니다.");
+            
+            if (i == noShow) {
+                continue;
+            }
+
+            sold++;
+            if (sold == max) {
+                System.out.println("금일 재료가 모두 소진되었습니다.");
+                break;
+            }
+        }
+    }
+}
