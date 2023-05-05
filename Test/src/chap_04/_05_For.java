399e3f9d6d55b93f8c86062051a301ff73c8b7fc
emhaki <mhmh779@naver.com>
2023-05-05 12:55:11 +0900

    For

--- Test/src/chap_04/_05_For.java
+++ Test/src/chap_04/_05_For.java
@@ -11,5 +11,10 @@ public class _05_For {
         for (int i = 5; i > 0; i--) {
             System.out.println(i);
         }
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+            System.out.println(sum);
+        }
     }
 }
