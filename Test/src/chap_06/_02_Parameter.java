0902b48cb77f65a49b00d947c5dceadea90c61cf
emhaki <mhmh779@naver.com>
2023-05-05 16:16:47 +0900

    Parameter

---
+++ Test/src/chap_06/_02_Parameter.java
@@ -0,0 +1,12 @@
+package chap_06;
+
+public class _02_Parameter {
+    public static void power(int a, int b) {
+        System.out.println(a * b);
+    }
+
+    public static void main(String[] args) {
+        // 전달값 Parameter
+        power(10, 20);
+    }
+}
